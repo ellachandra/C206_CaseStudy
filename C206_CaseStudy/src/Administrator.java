2ef8ccf8b113e158f511c3ea1511c20c6cdbd082
jiaan <jiaan@DESKTOP-LLKQN9M>
2023-07-24 00:12:07 +0800

    Test 2

---
+++ C206_CaseStudy/src/Administrator.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+
+/**
+ * @author jiaan
+ *
+ */
+public class Administrator extends User {
+	public Administrator(String userId, String name, String email) {
+        super(userId, name, email);
+    }
+
+}
--- C206_CaseStudy/src/Course.java
+++ C206_CaseStudy/src/Course.java
@@ -7,61 +7,35 @@
  *
  */
 public class Course {
-	private String courseTitle;
 	private String courseCode;
+	private String courseTitle;
 	private String courseDesc;
-	private String courseSchool;
-	private int courseCap;
+	private String schedule;
 	
-	public Course(String courseTitle, String courseCode, String courseDesc, String courseSchool, int courseCap) {
+	public Course(String courseCode, String courseTitle, String courseDesc, String schedule) {
 		
-		this.courseTitle = courseTitle;
 		this.courseCode = courseCode;
-		this.courseDesc = courseDesc;
-		this.courseSchool = courseSchool;
-		this.courseCap = courseCap;
-	}
-
-	public String getCourseTitle() {
-		return courseTitle;
-	}
-
-	public void setCourseTitle(String courseTitle) {
 		this.courseTitle = courseTitle;
+		this.courseDesc = courseDesc;
+		this.schedule = schedule;
 	}
 
 	public String getCourseCode() {
 		return courseCode;
 	}
 
-	public void setCourseCode(String courseCode) {
-		this.courseCode = courseCode;
+	public String getCourseTitle() {
+		return courseTitle;
 	}
 
 	public String getCourseDesc() {
 		return courseDesc;
 	}
 
-	public void setCourseDesc(String courseDesc) {
-		this.courseDesc = courseDesc;
+	public String getSchedule() {
+		return schedule;
 	}
-
-	public String getCourseSchool() {
-		return courseSchool;
-	}
-
-	public void setCourseSchool(String courseSchool) {
-		this.courseSchool = courseSchool;
-	}
-
-	public  String showAvailability(boolean courseCap) {
-		String avail;
-
-		if (courseCap == true) {
-			avail = "Yes";
-		} else {
-			avail = "No";
-		}
-		return avail;
-	}
+	
+	
+	
 }
